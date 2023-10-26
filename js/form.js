c24b928af633c52804c14b204d65b7ce8169401f
pamonge <pablomonge55@gmail.com>
2023-10-26 10:22:25 -0300

    FIX: Corregir el codigo para poder enviar correos

--- js/form.js
+++ js/form.js
@@ -1,14 +1,16 @@
-import { transporter } from "./mailer";
-
-const mail = document.getElementById('mail');
-const mensaje = document.getElementById('textareaMensaje')
-const checkNoRobot = document.getElementById('checkNoSoyRobot');
-const checkTermYPol = document.getElementById('checkTermYPolPriv');
 const formulario = document.getElementById('formulario');
 
-formulario.addEventListener('submit', e => {
+formulario.addEventListener('submit', async e => {
+    
     e.preventDefault();
-    let validMail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+    const mail = document.getElementById('mail');
+    const mensaje = document.getElementById('textareaMensaje')
+    const checkNoRobot = document.getElementById('checkNoSoyRobot');
+    const checkTermYPol = document.getElementById('checkTermYPolPriv');
+
+    let validMail = ;
+    
     if (!validMail.test(mail.value)) {
         mail.value = ' ';
         document.getElementById('mail').focus();
@@ -30,32 +32,16 @@ formulario.addEventListener('submit', e => {
         alert('Debe aceptar los terminos y condiciones...');
         return;
     }
+    
+    const response = await fetch('http://localhost:5500/send-email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email: mail, message: mensaje }),
+    });
 
-    async function main() {
-        // send mail with defined transport object
-        const info = await transporter.sendMail({
-          from: mail.value,
-          to: 'cuentamiaparacursos@gmail.com',// soporte@bytecrafters.com
-          subject: "Soporte ByteCrafters", // Subject line
-          text: mensaje.value, // plain text body
-          html: "<b>Hello world?</b>", // html body
-        });
-      
-        console.log("Message sent: %s", info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      
-        //
-        // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-        //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-        //       <https://github.com/forwardemail/preview-email>
-        //
-      }
-      
-      main().catch(console.error);
+    const data = await response.text();
+    console.log(data);
+
 } )
-
-
-console.log('mail');
-console.log('mensaje');
-console.log('checkNoRobot');
-console.log('checkTermYPol');
